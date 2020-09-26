a85fae2d366f633280bbb56f8b2660043c1bc5c6
Veigal <53204081+Veigal@users.noreply.github.com>
2020-09-25 22:15:37 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -2,24 +2,88 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
+
+#define QTD_PRODUTOS 6
+#define QTD_QUALIDADE 5
+#define QTD_TAMANHO 3
+#define TAM_NOME 10
 
 //Struct produto
 typedef struct Produto {
-    char nome[20];
-    int qualidade;
     char tamanho;
+    char nome[TAM_NOME];
+    int qualidade;
 } TProduto;
+
+int geraNumeroAleatorio(int qtdValores) {
+    time_t t;
+    srand((unsigned) time(&t));
+    return rand() % (qtdValores);
+}
 
 void consumidor(int* file_pipes){
     int data_processed;
     TProduto *produtoRecebe;
     produtoRecebe = (TProduto*)malloc(2 * sizeof(TProduto));
+    int sleepTime;
 
-    data_processed = read(file_pipes[0], produtoRecebe, sizeof(produtoRecebe));
-    printf("Read %d bytes: %s\n", data_processed, produtoRecebe->nome);
-    data_processed = read(file_pipes[0], produtoRecebe, sizeof(produtoRecebe));
-    printf("Read %d bytes: %s\n", data_processed, produtoRecebe->nome);
+
+    while(1){
+        sleepTime = 1;
+        data_processed = read(file_pipes[0], produtoRecebe,sizeof(produtoRecebe->nome) + sizeof(produtoRecebe->qualidade) + sizeof(produtoRecebe->tamanho));
+
+        if (produtoRecebe->tamanho == 'G'){
+            sleepTime ++;
+        }
+        sleep(sleepTime);
+        printf("Read %d bytes: %s - %c - %d\n", data_processed, produtoRecebe->nome, produtoRecebe->tamanho, produtoRecebe->qualidade);
+    }
     exit(EXIT_SUCCESS);
+}
+
+void defineNomeProduto(char* nomeProduto){
+    char nomesProdutos[QTD_PRODUTOS][TAM_NOME] = {"camiseta", "moleton", "jeans", "camisa", "casaco", "bermuda"};
+    int i = geraNumeroAleatorio(QTD_PRODUTOS);
+    strcpy(nomeProduto, nomesProdutos[i]);
+}
+
+char defineTamanhoProduto(){
+    int i = geraNumeroAleatorio(QTD_TAMANHO);
+
+    switch (i)
+    {   
+    case 0:
+        sleep(1);
+        return 'P';
+        break;
+    case 1:
+        sleep(1);
+        return 'M';
+        break;
+    case 2:
+        sleep(2);
+        return 'G';
+        break;
+    }
+}
+
+int defineQualidadeProduto(){
+    int i = geraNumeroAleatorio(QTD_QUALIDADE) + 1;
+
+    switch (i)
+    {
+    case 4:
+        sleep(1);
+        break;
+    case 5:
+        sleep(2);
+        break;
+    default:
+        break;
+    }
+
+    return (i);
 }
 
 void produtor(int* file_pipes){
@@ -27,12 +91,13 @@ void produtor(int* file_pipes){
     TProduto *produto;
     produto = (TProduto*)malloc(2 * sizeof(TProduto));
 
-    strcpy(produto->nome, "testess");
-    data_processed = write(file_pipes[1], produto, sizeof(produto));
-    printf("Wrote %d bytes\n", data_processed);
-    strcpy(produto->nome, "testess2222");
-    data_processed = write(file_pipes[1], produto, sizeof(produto));
-    printf("Wrote %d bytes\n", data_processed);
+    while(1){
+        defineNomeProduto(produto->nome);
+        produto->tamanho = defineTamanhoProduto();
+        produto->qualidade = defineQualidadeProduto();    
+        data_processed = write(file_pipes[1], produto, sizeof(produto->nome) + sizeof(produto->tamanho) + sizeof(produto->qualidade) );
+        printf("Wrote %d bytes\n", data_processed);
+    }
     exit(EXIT_SUCCESS);
 }
 
@@ -67,8 +132,8 @@ void criaFilhosProdutores(int* file_pipes, int numProcessos){
 }
 
 int main() {
-
     int file_pipes[2];
+
     if(pipe(file_pipes) != 0)
         exit(EXIT_FAILURE);
 
